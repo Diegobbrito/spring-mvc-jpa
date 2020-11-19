08eebe4fb76297a000ce8167449b75260802a7ca
Diego Brito <dibbrito@gmail.com>
2020-11-19 17:36:28 -0300

    Config

---
+++ src/main/java/com/dev/spring/config/SpringDataConfig.java
@@ -0,0 +1,12 @@
+package com.dev.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class  SpringDataConfig {
+
+}
